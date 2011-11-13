ac35e63e6e23f8600125ebb7459d1e2c5c6d1e44
Mark Webb-Johnson <mark@webb-johnson.net>
2011-11-13 20:50:12 +0800

    Major rework - maybe some breakages - inclusion of encrypted NET MSG layer, running but buggy

---
+++ vehicle/OVMS.X/net_msg.c
@@ -0,0 +1,271 @@
+/*
+;    Project:       Open Vehicle Monitor System
+;    Date:          16 October 2011
+;
+;    Changes:
+;    1.0  Initial release
+;
+;    (C) 2011  Michael Stegen / Stegen Electronics
+;    (C) 2011  Mark Webb-Johnson
+;    (C) 2011  Sonny Chen @ EPRO/DX
+;
+; Permission is hereby granted, free of charge, to any person obtaining a copy
+; of this software and associated documentation files (the "Software"), to deal
+; in the Software without restriction, including without limitation the rights
+; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+; copies of the Software, and to permit persons to whom the Software is
+; furnished to do so, subject to the following conditions:
+;
+; The above copyright notice and this permission notice shall be included in
+; all copies or substantial portions of the Software.
+;
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+; THE SOFTWARE.
+*/
+
+#include <stdio.h>
+#include <usart.h>
+#include <string.h>
+#include <stdlib.h>
+#include "ovms.h"
+#include "net.h"
+#include "can.h"
+#include "net_msg.h"
+#include "crypt_base64.h"
+#include "crypt_md5.h"
+#include "crypt_hmac.h"
+#include "crypt_rc4.h"
+
+// NET_MSG data
+#define TOKEN_SIZE 22
+#pragma udata
+char token[23] = {0};
+char digest[MD5_SIZE];
+char serverok = 0;
+char net_msg_scratchpad[100];
+
+#pragma udata TX_CRYPTO
+RC4_CTX2 tx_crypto2;
+#pragma udata RX_CRYPTO
+RC4_CTX2 rx_crypto2;
+#pragma udata
+RC4_CTX1 tx_crypto1;
+RC4_CTX1 rx_crypto1;
+
+void net_msg_init(void)
+  {
+  }
+
+void net_msg_disconnected(void)
+  {
+  serverok = 0;
+  }
+
+// Start to send a net msg
+void net_msg_start(void)
+  {
+  delay100(5);
+  net_puts_rom("AT+CIPSEND\r");
+  delay100(2);
+  }
+
+// Finish sending a net msg
+void net_msg_send(void)
+  {
+  net_puts_rom("\x1a");
+  }
+
+// Encode the message in net_scratchpad and start the send process
+void net_msg_encode_puts(void)
+  {
+  int k = strlen(net_scratchpad);
+
+  RC4_crypt(&tx_crypto1, &tx_crypto2, net_scratchpad, k);
+  base64encodesend(net_scratchpad,k);
+  net_puts_rom("\r\n");
+  }
+
+// Register to the NET OVMS server
+void net_msg_register(void)
+  {
+  char k;
+  char *p;
+  unsigned int sr;
+
+  // Make a (semi-)random client token
+  sr = TMR0L*256;
+  sr += TMR0H;
+  for (k=0;k<8;k++)
+    {
+    sr += can_databuffer[k];
+    }
+  srand(sr);
+  for (k=0;k<TOKEN_SIZE;k++)
+    {
+    token[k] = cb64[rand()%64];
+    }
+  token[TOKEN_SIZE] = 0;
+
+  p = par_get(PARAM_NETPASS1);
+  hmac_md5(token, TOKEN_SIZE, p, strlen(p), digest);
+
+  net_puts_rom("MP-C 0 ");
+  net_puts_ram(token);
+  net_puts_rom(" ");
+  base64encodesend(digest, MD5_SIZE);
+  net_puts_rom(" ");
+  p = par_get(PARAM_MYID);
+  net_puts_ram(p);
+  net_puts_rom("\r\n");
+  }
+
+void net_msg_stat(void)
+  {
+  char *p;
+
+  strcpypgm2ram(net_scratchpad,(char const rom far*)"MP-0 S");
+  p = par_get(PARAM_MILESKM);
+  sprintf(net_msg_scratchpad,(rom far char*)"%d,%s,%d,%d,",car_SOC,p,car_linevoltage,car_chargecurrent);
+  strcat(net_scratchpad,net_msg_scratchpad);
+  switch (car_chargestate)
+    {
+    case 0x01:
+      strcatpgm2ram(net_scratchpad,(char const rom far*)"charging,"); // Charge State Charging
+      break;
+    case 0x02:
+      strcatpgm2ram(net_scratchpad,(char const rom far*)"topoff,"); // Topping off
+      break;
+    case 0x04:
+      strcatpgm2ram(net_scratchpad,(char const rom far*)"done,"); // Done
+      break;
+    default:
+      strcatpgm2ram(net_scratchpad,(char const rom far*)"stopped,"); // Stopped
+    }
+  switch (car_chargemode)
+    {
+    case 0x00:
+      strcatpgm2ram(net_scratchpad,(char const rom far*)"standard,"); // Charge Mode Standard
+      break;
+    case 0x01:
+      strcatpgm2ram(net_scratchpad,(char const rom far*)"storage,"); // Storage
+      break;
+    case 0x03:
+      strcatpgm2ram(net_scratchpad,(char const rom far*)"range,"); // Range
+      break;
+    case 0x04:
+      strcatpgm2ram(net_scratchpad,(char const rom far*)"performance,"); // Performance
+    default:
+      strcatpgm2ram(net_scratchpad,(char const rom far*)",");
+    }
+  if (*p == 'M') // Kmh or Miles
+    sprintf(net_msg_scratchpad, (rom far char*)"%u,", car_idealrange);
+  else
+    sprintf(net_msg_scratchpad, (rom far char*)"%u,", (unsigned int) ((float) car_idealrange * 1.609));
+  strcat(net_scratchpad,net_msg_scratchpad);
+  if (*p == 'M') // Kmh or Miles
+    sprintf(net_msg_scratchpad, (rom far char*)"%u", car_estrange);
+  else
+    sprintf(net_msg_scratchpad, (rom far char*)"%u", (unsigned int) ((float) car_estrange * 1.609));
+  strcat(net_scratchpad,net_msg_scratchpad);
+  net_msg_encode_puts();
+  }
+
+void net_msg_gps(void)
+  {
+  strcpypgm2ram(net_scratchpad,(char const rom far*)"MP-0 L");
+  format_latlon(car_latitude,net_msg_scratchpad);
+  strcat(net_scratchpad,net_msg_scratchpad);
+  strcatpgm2ram(net_scratchpad,(char const rom far*)",");
+  format_latlon(car_longitude,net_msg_scratchpad);
+  strcat(net_scratchpad,net_msg_scratchpad);
+  net_msg_encode_puts();
+  }
+
+void net_msg_server_welcome(char *msg)
+  {
+  // The server has sent a welcome (token <space> base64digest)
+  char *d,*x,*p;
+  int k;
+
+  for (d=msg;(*d != 0)&&(*d != ' ');d++) ;
+  if (*d != ' ') return;
+  *d = 0; x = d+1;
+
+  // At this point, <msg> is token, and <x> is base64digest
+  // (both null-terminated)
+
+  // Check for token-replay attack
+  if (strcmp(token,msg)==0)
+    return; // Server is using our token!
+
+  // Validate server token
+  base64decode(x, net_msg_scratchpad); // Decode server digest to scratchpad
+  p = par_get(PARAM_NETPASS1);
+  hmac_md5(msg, strlen(msg), p, strlen(p), digest);
+  if (memcmp((const far void*)digest, (const far void*)net_msg_scratchpad, MD5_SIZE)!=0)
+    return; // Invalid server digest
+
+  // Ok, at this point, our token is ok
+  strcpy(net_scratchpad,token);
+  strcat(net_scratchpad,msg);
+  hmac_md5(net_scratchpad,strlen(net_scratchpad),p,strlen(p),digest);
+
+  // Setup, and prime the rx and tx cryptos
+  RC4_setup(&rx_crypto1, &rx_crypto2, digest, MD5_SIZE);
+  for (k=0;k<1024;k++)
+    {
+    net_scratchpad[0] = 0;
+    RC4_crypt(&rx_crypto1, &rx_crypto2, net_scratchpad, 1);
+    }
+  RC4_setup(&tx_crypto1, &tx_crypto2, digest, MD5_SIZE);
+  for (k=0;k<1024;k++)
+    {
+    net_scratchpad[0] = 0;
+    RC4_crypt(&tx_crypto1, &tx_crypto2, net_scratchpad, 1);
+    }
+
+  serverok = 1;
+  }
+
+// Receive a NET msg from the OVMS server
+void net_msg_in(char* msg)
+  {
+  int k;
+
+  if (serverok == 0)
+    {
+    if (memcmppgm2ram(msg, (char const rom far*)"MP-S 0 ", 7) == 0)
+      {
+      net_msg_server_welcome(msg+7);
+      }
+    return; // otherwise ignore it
+    }
+
+  // Ok, we've got an encrypted message waiting for work.
+  k = base64decode(msg,net_scratchpad);
+  RC4_crypt(&rx_crypto1, &rx_crypto2, net_scratchpad, k);
+  if (memcmppgm2ram(net_scratchpad, (char const rom far*)"MP-0 S", 6) == 0)
+    {
+    msg = net_scratchpad+6;
+    switch (*msg)
+      {
+      case 'A': // PING
+        strcpypgm2ram(net_scratchpad,(char const rom far*)"MP-0 Ca");
+        net_msg_start();
+        net_msg_encode_puts();
+        net_msg_send();
+        break;
+      case 'Z': // PEER connection
+        net_msg_start();
+        net_msg_stat();
+        net_msg_gps();
+        net_msg_send();
+        break;
+      }
+    }
+  }
