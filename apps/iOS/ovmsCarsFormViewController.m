1638b3c792a122a3f5eb8736738c1f8fe787fce7
Mark Webb-Johnson <mark@webb-johnson.net>
2011-11-25 13:30:30 +0800

    UI image picker for car image

--- apps/iOS/ovmsCarsFormViewController.m
+++ apps/iOS/ovmsCarsFormViewController.m
@@ -9,10 +9,14 @@
 #import "ovmsCarsFormViewController.h"
 
 @implementation ovmsCarsFormViewController
+
+@synthesize carImages;
+
 @synthesize vehicleid;
 @synthesize vehiclelabel;
-@synthesize vehiclepassword;
-@synthesize vehiclepicker;
+@synthesize vehicleNetPass;
+@synthesize vehicleUserPass;
+@synthesize vehicleImage;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -40,21 +44,31 @@
 }
 */
 
-/*
+
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+  self.carImages = [[NSMutableArray alloc] initWithCapacity:100];
+  NSString *bundleRoot = [[NSBundle mainBundle] bundlePath];
+  NSArray *dirContents = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:bundleRoot error:nil];
+  for (NSString *tString in dirContents)
+    {
+    if ([tString hasPrefix:@"car_"] && [tString hasSuffix:@".png"])
+      {
+      [self.carImages addObject: tString];
+      }
+    }
 }
-*/
 
 - (void)viewDidUnload
 {
   [self setVehicleid:nil];
   [self setVehiclelabel:nil];
-  [self setVehiclepassword:nil];
-  [self setVehiclepicker:nil];
-    [super viewDidUnload];
+  [self setVehicleNetPass:nil];
+  [self setVehicleUserPass:nil];
+  [self setVehicleImage:nil];
+  [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
 }
@@ -63,6 +77,47 @@
 {
     // Return YES for supported orientations
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (IBAction)textFieldReturn:(id)sender
+{
+  [sender resignFirstResponder];
+}
+
+
+#pragma mark -
+#pragma mark PickerView DataSource
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+  return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+  return [carImages count];
+}
+
+- (CGFloat)pickerView:(UIPickerView *)pickerView rowHeightForComponent:(NSInteger)component
+{
+  return 151;
+}
+
+- (UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row
+          forComponent:(NSInteger)component reusingView:(UIView *)view
+{
+  UIImageView *newview = [[UIImageView alloc] initWithImage:[UIImage imageNamed:[carImages objectAtIndex:row]]];
+  newview.frame = CGRectMake(0, 0, 320, 151);
+  newview.contentMode =  UIViewContentModeScaleAspectFit;
+  return newview;
+}
+
+#pragma mark -
+#pragma mark PickerView Delegate
+
+-(void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row
+      inComponent:(NSInteger)component
+{
 }
 
 @end
