cf2da7cc82fa95cff2581f0393e08b8ade5d7ceb
Mark Webb-Johnson <mark@webb-johnson.net>
2012-10-29 21:35:34 +0800

    Volt/Ampera work-in-progress

--- vehicle/OVMS.X/can_voltampera.c
+++ vehicle/OVMS.X/can_voltampera.c
@@ -77,6 +77,8 @@ void can_initialise(void)
   car_type[0] = 'V'; // Car is type VA - Volt/Ampera
   car_type[1] = 'A';
   car_type[2] = 0;
+  car_type[3] = 0;
+  car_type[4] = 0;
 
   CANCON = 0b10010000; // Initialize CAN
   while (!CANSTATbits.OPMODE2); // Wait for Configuration mode
@@ -87,6 +89,7 @@ void can_initialise(void)
   RXM0SIDL = 0b10100000;
   RXM0SIDH = 0b11111111;	// Set Mask0 to 0x7fd
 
+  // Not yet used for VA
   RXF0SIDL = 0b00000000;	// Setup Filter0 and Mask so that only CAN ID 0x100 and 0x102 will be accepted
   RXF0SIDH = 0b00100000;	// Set Filter0 to 0x100
 
@@ -95,14 +98,24 @@ void can_initialise(void)
   RXM1SIDL = 0b11100000;
   RXM1SIDH = 0b11111111;	// Set Mask1 to 0x7ff
 
-  RXF2SIDL = 0b10000000;	// Setup Filter2 so that CAN ID 0x344 will be accepted
-  RXF2SIDH = 0b01101000;
+  // Filters: low byte bits 7..5 are the low 3 bits of the filter, and bits 4..0 are all zeros
+  //          high byte bits 7..0 are the high 8 bits of the filter
+  //          So total is 11 bits
 
-  RXF3SIDL = 0b01000000;	// Setup Filter3 so that CAN ID 0x402 will be accepted
-  RXF3SIDH = 0b10000000;
+  // N.B. This is a very wasteful use of filters, but good enough for proof-of-concept
+  //      Final implementation will most likely have to use masks to better affect
+  
+  // Filter 2: Used for ID#0x206 (SOC)
+  RXF2SIDL = 0b11000000;	// Setup Filter2 so that CAN ID 0x206 will be accepted
+  RXF2SIDH = 0b01000000;
 
-  RXF4SIDL = 0b00000000;        // Setup Filter4 so that CAN ID 0x400 will be accepted
-  RXF4SIDH = 0b10000000;
+  // Filter 3: Used for ID#0x4E9
+  RXF3SIDL = 0b00100000;	// Setup Filter3 so that CAN ID 0x4E9 will be accepted
+  RXF3SIDH = 0b10011101;
+
+  // Filter 4: Used for ID#0x514
+  RXF4SIDL = 0b10000000;        // Setup Filter4 so that CAN ID 0x514 will be accepted
+  RXF4SIDH = 0b10100010;
 
   BRGCON1 = 0; // SET BAUDRATE to 1 Mbps
   BRGCON2 = 0xD2;
@@ -134,7 +147,7 @@ void can_initialise(void)
 // This function is an entry point from the main() program loop, and
 // gives the CAN framework an opportunity to poll for data.
 //
-void can_poll0(void)                // CAN ID 100 and 102
+void can_poll0(void)
   {
   can_datalength = RXB0DLC & 0x0F; // number of received bytes
   can_databuffer[0] = RXB0D0;
@@ -149,7 +162,7 @@ void can_poll0(void)                // CAN ID 100 and 102
   RXB0CONbits.RXFUL = 0; // All bytes read, Clear flag
   }
 
-void can_poll1(void)                // CAN ID 344 and 402
+void can_poll1(void)
   {
   unsigned char CANctrl;
 
@@ -165,6 +178,30 @@ void can_poll1(void)                // CAN ID 344 and 402
 
   CANctrl=RXB1CON;		// copy CAN RX1 Control register
   RXB1CONbits.RXFUL = 0; // All bytes read, Clear flag
+
+  if ((CANctrl & 0x07) == 2)    	// Acceptance Filter 2 (RXF2) = CAN ID 0x206
+    {
+    // SOC
+    // For the SOC, each 4,000 is 1kWh. Assuming a 16.1kWh battery, 1% SOC is 644 decimal bytes
+    // The SOC itself is d1<<8 + d2
+    car_SOC = (char)((can_databuffer[1]+((unsigned int) can_databuffer[0] << 8))/644);
+    }
+  else if ((CANctrl & 0x07) == 3)           // Acceptance Filter 3 (RXF3) = CAN ID 4E9
+    {
+    // The VIN can be constructed by taking the number "1" and converting the CAN IDs 4E9 and 514 to ASCII.
+    // So with "4E1 4255313032363839" and "514 4731524436453436",
+    // you would end up with 42 55 31 30 32 36 38 39 47 31 52 44 36 45 34 36,
+    // where 42 is ASCII for B, 55 is U, etc.
+    for (k=0;k<8;k++)
+      car_vin[k] = can_databuffer[k];
+    }
+  else if ((CANctrl & 0x07) == 4)           // Acceptance Filter 4 (RXF4) = CAN ID 514
+    {
+    for (k=0;k<8;k++)
+      car_vin[k+8] = can_databuffer[k];
+    car_vin[16] = 0;
+    }
+
   }
 
 ////////////////////////////////////////////////////////////////////////
