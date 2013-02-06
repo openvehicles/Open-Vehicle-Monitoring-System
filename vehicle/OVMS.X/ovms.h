1447eeff3553c2f0bc127ac92f58f2fced34f7f5
Michael Balzer <balzer@expeedo.de>
2013-02-06 23:25:57 +0100

    - Added documentation info on car_doors bits

--- vehicle/OVMS.X/ovms.h
+++ vehicle/OVMS.X/ovms.h
@@ -82,68 +82,74 @@ extern unsigned char car_chargemode; // 0=standard, 1=storage, 3=range, 4=perfor
 extern unsigned char car_charge_b4; // B4 byte of charge state
 extern unsigned char car_chargekwh; // KWh of charge
 
+
+//
+// CAR STATUS BITS:
+// see developer manual on "Virtual Vehicle / Environment" for more info
+//
+
 extern unsigned char car_doors1;
 typedef struct {
-  unsigned FrontLeftDoor:1;
-  unsigned FrontRightDoor:1;
-  unsigned ChargePort:1;
-  unsigned PilotSignal:1;
-  unsigned Charging:1;
-  unsigned :1;
-  unsigned HandBrake:1;
-  unsigned CarON:1;
+  unsigned FrontLeftDoor:1;     // 0x01
+  unsigned FrontRightDoor:1;    // 0x02
+  unsigned ChargePort:1;        // 0x04
+  unsigned PilotSignal:1;       // 0x08
+  unsigned Charging:1;          // 0x10
+  unsigned :1;                  // 0x20
+  unsigned HandBrake:1;         // 0x40
+  unsigned CarON:1;             // 0x80
 } car_doors1bits_t;
 #define car_doors1bits (*((car_doors1bits_t*)&car_doors1))
 
 extern unsigned char car_doors2;
 typedef struct {
-  unsigned :1;
-  unsigned :1;
-  unsigned :1;
-  unsigned CarLocked:1;
-  unsigned ValetMode:1;
-  unsigned Bonnet:1;
-  unsigned Trunk:1;
-  unsigned :1;
+  unsigned :1;                  // 0x01
+  unsigned :1;                  // 0x02
+  unsigned :1;                  // 0x04
+  unsigned CarLocked:1;         // 0x08
+  unsigned ValetMode:1;         // 0x10
+  unsigned Bonnet:1;            // 0x20
+  unsigned Trunk:1;             // 0x40
+  unsigned :1;                  // 0x80
 } car_doors2bits_t;
 #define car_doors2bits (*((car_doors2bits_t*)&car_doors2))
 
 extern unsigned char car_doors3;
 typedef struct {
-  unsigned :1;
-  unsigned CoolingPump:1;
-  unsigned :1;
-  unsigned :1;
-  unsigned :1;
-  unsigned :1;
-  unsigned :1;
-  unsigned :1;
+  unsigned :1;                  // 0x01
+  unsigned CoolingPump:1;       // 0x02
+  unsigned :1;                  // 0x04
+  unsigned :1;                  // 0x08
+  unsigned :1;                  // 0x10
+  unsigned :1;                  // 0x20
+  unsigned :1;                  // 0x40
+  unsigned :1;                  // 0x80
 } car_doors3bits_t;
 #define car_doors3bits (*((car_doors3bits_t*)&car_doors3))
 
 extern unsigned char car_doors4;
 typedef struct {
-  unsigned :1;
-  unsigned :1;
-  unsigned AlarmSounds:1;
-  unsigned :1;
-  unsigned :1;
-  unsigned :1;
-  unsigned :1;
-  unsigned :1;
+  unsigned :1;                  // 0x01
+  unsigned :1;                  // 0x02
+  unsigned AlarmSounds:1;       // 0x04
+  unsigned :1;                  // 0x08
+  unsigned :1;                  // 0x10
+  unsigned :1;                  // 0x20
+  unsigned :1;                  // 0x40
+  unsigned :1;                  // 0x80
 } car_doors4bits_t;
 #define car_doors4bits (*((car_doors4bits_t*)&car_doors4))
 
 extern unsigned char car_doors5;
 typedef struct {
-  unsigned RearLeftDoor:1;
-  unsigned RearRightDoor:1;
-  unsigned Frunk:1;
-  unsigned :1;
-  unsigned Charging12V:1;
-  unsigned :1;
-  unsigned :1;
-  unsigned :1;
+  unsigned RearLeftDoor:1;      // 0x01
+  unsigned RearRightDoor:1;     // 0x02
+  unsigned Frunk:1;             // 0x04
+  unsigned :1;                  // 0x08
+  unsigned Charging12V:1;       // 0x10
+  unsigned :1;                  // 0x20
+  unsigned :1;                  // 0x40
+  unsigned :1;                  // 0x80
 } car_doors5bits_t;
 #define car_doors5bits (*((car_doors5bits_t*)&car_doors5))
 
