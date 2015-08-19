d53f4ea9cd61cf59599eaea3672d6099efa5f7a0
Michael Balzer <balzer@expeedo.de>
2015-08-19 20:18:35 +0200

    Client: added Edimax smart plug control scripts

---
+++ client/edimax/edimax_get.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Read config:
+source edimax_config.txt || exit
+
+# Get Edimax state:
+RESULT=$( ( curl --silent --data @- http://${EDIMAX_USER}:${EDIMAX_PASS}@${EDIMAX_IP}:10000/smartplug.cgi --output - <<-__XML__
+	<?xml version="1.0" encoding="utf-8"?>
+	<SMARTPLUG id="edimax"><CMD id="get"><Device.System.Power.State></Device.System.Power.State></CMD></SMARTPLUG>
+__XML__
+) | sed -e 's:.*<Device.System.Power.State>\([^<]*\).*:\1:g' )
+
+# Output result:
+echo -n ${RESULT}
---
+++ client/edimax/edimax_switch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Read config:
+source edimax_config.txt || exit
+
+# Get switch state argument:
+TOSTATE=$1
+if [[ "${TOSTATE}" = "" ]] ; then
+	echo "Usage: $0 [ON|OFF]"
+	exit
+fi
+
+# Switch Edimax and read result:
+RESULT=$( ( curl --silent --data @- http://${EDIMAX_USER}:${EDIMAX_PASS}@${EDIMAX_IP}:10000/smartplug.cgi --output - <<-__XML__
+	<?xml version="1.0" encoding="utf-8"?>
+	<SMARTPLUG id="edimax"><CMD id="setup"><Device.System.Power.State>${TOSTATE}</Device.System.Power.State></CMD></SMARTPLUG>
+__XML__
+) | sed -e 's:.*<CMD id="setup">\([^<]*\).*:\1:g' )
+
+# Output result:
+echo ${RESULT}
