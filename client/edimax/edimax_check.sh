f888ed71525281ce7e2c5c16557032187c81dfa1
Michael Balzer <balzer@expeedo.de>
2015-10-03 19:01:51 +0200

    Client Edimax scripts: silent cron errors, syslog output

--- client/edimax/edimax_check.sh
+++ client/edimax/edimax_check.sh
@@ -6,22 +6,26 @@ source edimax_config.txt || exit
 # Read current SOC:
 SOC=$(./status.pl | sed 's:MP-0 S\([0-9]*\).*:\1:g')
 if [[ "$SOC" = "" ]] ; then
-	echo "SOC level unknown, aborting"
+	logger -p user.error "${0}: SOC level unknown, aborting"
+	echo "SOC level unknown, aborting" >&2
 	exit
 fi
 
 # Read current Edimax state:
 STATE=$(./edimax_get.sh)
 if [[ "$STATE" = "" ]] ; then
-	echo "Plug state unknown, aborting"
+	logger -p user.error "${0}: Plug state unknown, aborting"
+	echo "Plug state unknown, aborting" >&2
 	exit
 fi
 
 # Switch Edimax Smart Plug according to SOC level:
 if [[ $SOC -ge $SOC_OFF && $STATE = "ON" ]] ; then
+	logger "${0}: SOC level ${SOC}%, switching OFF"
 	echo -n "SOC level ${SOC}%, switching OFF... "
 	./edimax_switch.sh OFF
 elif [[ $SOC -le $SOC_ON && $STATE = "OFF" ]] ; then
+	logger "${0}: SOC level ${SOC}%, switching ON"
 	echo -n "SOC level ${SOC}%, switching ON... "
 	./edimax_switch.sh ON
 fi
