a9f765649ea3d1714da07771de9647b05ac8467b
Mark Webb-Johnson <mark@webb-johnson.net>
2016-03-14 18:58:28 +0800

    2.8.5 final changes

--- vehicle/OVMS.X/nbproject/Makefile-local-V2_RT_Production.mk
+++ vehicle/OVMS.X/nbproject/Makefile-local-V2_RT_Production.mk
@@ -14,11 +14,11 @@
 # You can invoke make with the values of the macros:
 # $ makeMP_CC="/opt/microchip/mplabc30/v3.30c/bin/pic30-gcc" ...  
 #
-PATH_TO_IDE_BIN=/Applications/microchip/mplabx/mplab_ide.app/Contents/Resources/mplab_ide/mplab_ide/modules/../../bin/
+PATH_TO_IDE_BIN=/Applications/microchip/mplabx/v3.25/mplab_ide.app/Contents/Resources/mplab_ide/mplab_ide/modules/../../bin/
 # Adding MPLAB X bin directory to path.
-PATH:=/Applications/microchip/mplabx/mplab_ide.app/Contents/Resources/mplab_ide/mplab_ide/modules/../../bin/:$(PATH)
+PATH:=/Applications/microchip/mplabx/v3.25/mplab_ide.app/Contents/Resources/mplab_ide/mplab_ide/modules/../../bin/:$(PATH)
 # Path to java used to run MPLAB X when this makefile was created
-MP_JAVA_PATH="/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home/bin/"
+MP_JAVA_PATH="/Applications/microchip/mplabx/v3.25/sys/java/jre1.8.0_65.jre/Contents/Home/bin/"
 OS_CURRENT="$(shell uname -s)"
 MP_CC="/Applications/microchip/mplabc18/v3.40/bin/mcc18"
 # MP_CPPC is not defined
@@ -26,7 +26,7 @@ MP_CC="/Applications/microchip/mplabc18/v3.40/bin/mcc18"
 MP_AS="/Applications/microchip/mplabc18/v3.40/bin/../mpasm/MPASMWIN"
 MP_LD="/Applications/microchip/mplabc18/v3.40/bin/mplink"
 MP_AR="/Applications/microchip/mplabc18/v3.40/bin/mplib"
-DEP_GEN=${MP_JAVA_PATH}java -jar "/Applications/microchip/mplabx/mplab_ide.app/Contents/Resources/mplab_ide/mplab_ide/modules/../../bin/extractobjectdependencies.jar"
+DEP_GEN=${MP_JAVA_PATH}java -jar "/Applications/microchip/mplabx/v3.25/mplab_ide.app/Contents/Resources/mplab_ide/mplab_ide/modules/../../bin/extractobjectdependencies.jar"
 MP_CC_DIR="/Applications/microchip/mplabc18/v3.40/bin"
 # MP_CPPC_DIR is not defined
 # MP_BC_DIR is not defined
