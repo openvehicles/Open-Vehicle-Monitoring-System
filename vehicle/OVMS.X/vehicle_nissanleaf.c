319cba86d8e4bb8159766d31e98b4cf47fb7d040
Tom Parker <tom@carrott.org>
2017-09-23 08:49:10 +1200

    Nissan Leaf: bump revision

--- vehicle/OVMS.X/vehicle_nissanleaf.c
+++ vehicle/OVMS.X/vehicle_nissanleaf.c
@@ -33,7 +33,7 @@
 #include "inputs.h"
 
 // Nissan Leaf module version:
-rom char nissanleaf_version[] = "1.4";
+rom char nissanleaf_version[] = "1.5";
 
 // Nissan Leaf capabilities:
 // - CMD_StartCharge (11)
